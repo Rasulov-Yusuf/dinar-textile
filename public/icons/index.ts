674916e93355d77170a50d2cf822a72989cf880b
Yusuf <yusuf.rasulof@gmail.com>
2025-11-21 22:21:30 +0500

    footer has done with responsive

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,1 +1,4 @@
-export {default as instagram} from './insta.svg'
+export {default as instagram} from './insta.svg';
+export {default as facebook} from './facebook.svg';
+export {default as youtube} from './youtube.svg';
+export {default as logo} from './logo-white.png'
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,62 @@
 import Image from "next/image";
-import logoWhite from "../../public/icons/logo-white.png";
-import { instagram } from "../../public/icons";
+import { instagram, youtube, facebook, logo } from "../../public/icons";
 
 import { links } from "@/asstes/data";
+import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-yellow text-white pt-[30px]">
       <div className="container">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
-          <div className="">
-            <Image src={logoWhite} alt="footer logo" />
+        <div className="flex flex-col md:flex-row justify-between gap-5">
+          <div className="flex flex-col justify-between grow gap-4">
+            <div>
+              <Image src={logo} alt="footer logo" />
+            </div>
+
+            <div className="flex gap-5 md:gap-x-10 lg:gap-x-24 flex-wrap">
+              <ul className="flex flex-col gap-[10px]">
+                <li className="opacity-60">Telefon</li>
+                <li>+998 (71) 231 86 01</li>
+              </ul>
+              <ul className="flex flex-col gap-[10px]">
+                <li className="opacity-60">Manzil</li>
+                <li>Toshkent shahridagi, Amir Temur ko'chasi, 45-uy.</li>
+              </ul>
+              <ul className="flex flex-col gap-[10px]">
+                <li className="opacity-60">Elektron manzil</li>
+                <li>info@dinar.uz</li>
+              </ul>
+            </div>
           </div>
 
-          <ul className="flex flex-col gap-[18px]">
-            {links.map((link) => (
-              <li key={link.id}>{link.link}</li>
-            ))}
-          </ul>
+          <div className="flex justify-between flex-wrap sm:flex-nowrap grow gap-5">
+            <ul className="flex flex-col gap-[18px]">
+              {links.map((link) => (
+                <li key={link.id}>{link.link}</li>
+              ))}
+            </ul>
 
-          <ul className="flex flex-col gap-[18px]">
-            <li className="flex items-center gap-2">
-              <div>
-                <Image src={instagram} alt="intagram icon" />
-              </div>
-              <p>Instagram</p>
-            </li>
-          </ul>
+            <div className="flex flex-col gap-[18px]">
+              <Link href={"#"} className="flex items-center gap-2">
+                <div className="basis-4 shrink-0">
+                  <Image src={instagram} alt="intagram icon" />
+                </div>
+                <p>Instagram</p>
+              </Link>
 
-          <div>
-            <ul className="flex flex-col gap-[10px]">
-              <li className="opacity-60">Telefon</li>
-              <li>+998 (71) 231 86 01</li>
-            </ul>
-            <ul className="flex flex-col gap-[10px]">
-              <li className="opacity-60">Manzil</li>
-              <li>Toshkent shahridagi, Amir Temur ko'chasi, 45-uy.</li>
-            </ul>
-            <ul className="flex flex-col gap-[10px]">
-              <li className="opacity-60">Elektron manzil</li>
-              <li>info@dinar.uz</li>
-            </ul>
+              <Link href={"#"} className="flex items-center gap-2">
+                <div className="basis-4 shrink-0">
+                  <Image src={facebook} alt="facebook icon" />
+                </div>
+                <p>Facebook</p>
+              </Link>
+              <Link href={"#"} className="flex items-center gap-2">
+                <div className="basis-4 shrink-0">
+                  <Image src={youtube} alt="youtube icon" />
+                </div>
+                <p>Youtube</p>
+              </Link>
+            </div>
           </div>
         </div>
 
