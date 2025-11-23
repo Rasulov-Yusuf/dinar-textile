711cdcc27691e21838cf31755872ef8877d49abc
𝕾𝖍𝖆𝖝𝖗𝖎𝖞𝖔𝖗 𝕿𝖚𝖗𝖌'𝖚𝖓𝖔𝖛 <shaxriyorturgunov000@gmail.com>
2025-11-23 22:41:26 +0500

    Yangi o'zgarish

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -7,9 +7,12 @@ export default function page() {
       <div className=" w-full  flex items-center justify-center text-center bg-[#CD333A]  min-h-[340px] font-semibold font-archivo bg-[url('/images/Union.png')] bg-cover bg-center bg-no-repeat">
         <h1 className=' font-archivo text-4xl uppercase text-white sm:text-[64px]'>Yangiliklar</h1>
       </div>
-       <div className='container'>
-            <Pagination/>
-       </div>
+      <div className='container'>
+        <div>
+
+        </div>
+        <Pagination />
+      </div>
     </div>
   )
 }
