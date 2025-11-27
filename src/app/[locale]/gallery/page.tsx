986dc866d63a5210214a277e6e8a2c8ae678523b
𝕾𝖍𝖆𝖝𝖗𝖎𝖞𝖔𝖗 𝕿𝖚𝖗𝖌'𝖚𝖓𝖔𝖛 <shaxriyorturgunov000@gmail.com>
2025-11-27 19:44:05 +0500

    news page

--- src/app/[locale]/gallery/page.tsx
+++ src/app/[locale]/gallery/page.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      Gallery page
+      <div className=" w-full  flex items-center justify-center text-center bg-[#CD333A]  min-h-[340px] font-semibold font-archivo bg-[url('/images/Union.png')] bg-cover bg-center bg-no-repeat">
+        <h1 className=' font-archivo text-4xl uppercase text-white sm:text-[64px]'>Galareya</h1>
+      </div>
+       <div className='container'>
+           
+       </div>
     </div>
   )
 }
--- src/asstes/data.ts
+++ src/asstes/data.ts
@@ -40,3 +40,4 @@ export const links = [
         url: '/orders'
     },
 ]
+
